24bb6dcfaf66e2d1d59ce8dbff1f5cb6281345be
Diego Cabrera <hola@LAB1-PC2>
2023-01-11 12:44:54 -0500

    Se agrega creación de archivo con File

--- src/manejoarchivos/ManejoArchivos.java
+++ src/manejoarchivos/ManejoArchivos.java
@@ -5,6 +5,9 @@
  */
 package manejoarchivos;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,6 +20,13 @@ public class ManejoArchivos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        File archivo = new File("archivo.txt");
+        try {
+            archivo.createNewFile();
+        } catch (IOException ex) {
+            System.out.println("Error en la creación del archivo");
+        }
+        System.out.println(archivo);
     }
     
 }
